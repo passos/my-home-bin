7023992fda1113bb09dabf39be35c9df9d6e0bfb
LiuJinyu <Liujinyu@wandoujia.com>
2014-12-11 17:31:30 +0800

    update

--- restart-fcitx.sh
+++ restart-fcitx.sh
@@ -1,5 +1,5 @@
 kill -9 `ps -ef|grep fcitx|grep -v grep |awk '{print $2}'`
 sleep 1
 echo 'start ....'
-fcitx -r --enable sogou-qimpanel
-sogou-qimpanel
+fcitx -r --enable sogou-qimpanel 2>&1 > /dev/null
+sogou-qimpanel 2>&1 > /dev/null
